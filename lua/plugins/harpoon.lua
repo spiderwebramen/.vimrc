97cdd85e8a6b81cac5fcaf3356bf7bd46081f68a
spiderwebramen <MCWGzxD@gmail.com>
2024-06-04 02:12:20 +0700

    harpoon update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,62 +5,47 @@ return {
     init = function()
         require("harpoon").setup()
     end,
-    keys = {
-        {
-            "<leader>ha",
-            function()
-                require("harpoon"):list():add()
-            end,
-            desc = "Add to List (Harpoon)",
-        },
-        {
-            "<leader>ho",
-            function()
-                require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-            end,
-            desc = "Toggle Quick Menu (Harpoon)",
-        },
-        {
-            "<leader>1",
-            function()
-                require("harpoon"):list():select(1)
-            end,
-            desc = "Select file 1 (Harpoon)",
-        },
-        {
-            "<leader>2",
-            function()
-                require("harpoon"):list():select(2)
-            end,
-            desc = "Select file 2 (Harpoon)",
-        },
-        {
-            "<leader>3",
-            function()
-                require("harpoon"):list():select(3)
-            end,
-            desc = "Select file 3 (Harpoon)",
-        },
-        {
-            "<leader>4",
-            function()
-                require("harpoon"):list():select(4)
-            end,
-            desc = "Select file 4 (Harpoon)",
-        },
-        {
-            "<leader>hp",
-            function()
-                require("harpoon"):list():prev()
-            end,
-            desc = "Go to prev file (Harpoon)",
-        },
-        {
-            "<leader>hn",
-            function()
-                require("harpoon"):list():next()
-            end,
-            desc = "Go to next file (Harpoon)",
-        },
-    },
+    keys = function()
+        local keys = {
+            {
+                "<leader>ha",
+                function()
+                    require("harpoon"):list():add()
+                end,
+                desc = "Harpoon File",
+            },
+            {
+                "<leader>ho",
+                function()
+                    require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+                end,
+                desc = "Harpoon Quick Menu",
+            },
+            {
+                "<leader>hp",
+                function()
+                    require("harpoon"):list():prev()
+                end,
+                desc = "Harpoon to Previous File",
+            },
+            {
+                "<leader>hn",
+                function()
+                    require("harpoon"):list():next()
+                end,
+                desc = "Harpoon to Next File",
+            },
+        }
+
+        for i = 1, 5 do
+            table.insert(keys, {
+                "<leader>" .. i,
+                function()
+                    require("harpoon"):list():select(i)
+                end,
+                desc = "Harpoon to File " .. 1,
+            })
+        end
+        return keys
+    end,
 }
