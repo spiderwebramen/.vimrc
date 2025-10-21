608c50f2d5ad630a11a19742f86532c809160f9a
spiderwebramen <MCWGzxD@gmail.com>
2025-10-21 22:05:24 +0700

    blink.cmp preselect false

--- init.lua
+++ init.lua
@@ -740,6 +740,7 @@ require('lazy').setup({
                 -- By default, you may press `<c-space>` to show the documentation.
                 -- Optionally, set `auto_show = true` to show the documentation after a delay.
                 documentation = { auto_show = false, auto_show_delay_ms = 500 },
+                list = { selection = { preselect = false } },
             },
 
             sources = {
