2874865b8dd3c9c3d779860ba7c6819abc619032
spiderwebramen <MCWGzxD@gmail.com>
2025-10-31 11:19:09 +0700

    harpoon

--- init.lua
+++ init.lua
@@ -148,6 +148,44 @@ require('lazy').setup({
         end,
     },
 
+    {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local harpoon = require 'harpoon'
+            harpoon:setup()
+
+            vim.keymap.set('n', '<leader>ha', function()
+                harpoon:list():add()
+            end)
+            vim.keymap.set('n', '<leader>he', function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end)
+
+            vim.keymap.set('n', '<leader>1', function()
+                harpoon:list():select(1)
+            end)
+            vim.keymap.set('n', '<leader>2', function()
+                harpoon:list():select(2)
+            end)
+            vim.keymap.set('n', '<leader>3', function()
+                harpoon:list():select(3)
+            end)
+            vim.keymap.set('n', '<leader>4', function()
+                harpoon:list():select(4)
+            end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set('n', '<leader>h[', function()
+                harpoon:list():prev()
+            end)
+            vim.keymap.set('n', '<leader>h]', function()
+                harpoon:list():next()
+            end)
+        end,
+    },
+
     'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
     -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
